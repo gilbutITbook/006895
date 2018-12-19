521c01e95503b79c24e867b9b1b5b4fce01a990a
leebs <leebs@DESKTOP-3FAS477>
2018-12-19 12:32:25 +0900

    교재 소스 코드 최초 업로드

--- bookShop01/src/main/java/com/bookshop01/goods/service/GoodsServiceImpl.java
+++ bookShop01/src/main/java/com/bookshop01/goods/service/GoodsServiceImpl.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
