d6c04b21d6812caec408d976cdfe38f129283728
leebs <leebs@DESKTOP-3FAS477>
2019-01-31 18:06:56 +0900

    교재 소스 코드 최초 업로드

--- pro07/src/sec02/ex01/MemberServlet.java
+++ pro07/src/sec02/ex01/MemberServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 
 
 
-@WebServlet("/member2")
+/*@WebServlet("/member2")*/
 public class MemberServlet extends HttpServlet {
 	public void doGet(HttpServletRequest request,HttpServletResponse response)  throws ServletException, IOException{
       response.setContentType("text/html;charset=utf-8");
