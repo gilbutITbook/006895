8c549331eaab5a59b87472dd9e1945ca16acc6d1
leebs <leebs@DESKTOP-3FAS477>
2019-02-10 14:53:44 +0900

    web.xml에 필터 기능 설정

--- pro10/src/sec03/ex01/LoginTest.java
+++ pro10/src/sec03/ex01/LoginTest.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class LoginTest
  */
-/*@WebServlet("/login")*/
+@WebServlet("/login")
 public class LoginTest extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
