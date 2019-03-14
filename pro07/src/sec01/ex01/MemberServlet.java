6013a5d50b557e27e63d1df04a49fc5bb217adc4
leebs <leebs@DESKTOP-3FAS477>
2019-03-14 13:34:37 +0900

    소스 수정

--- pro07/src/sec01/ex01/MemberServlet.java
+++ pro07/src/sec01/ex01/MemberServlet.java
@@ -3,7 +3,6 @@ package sec01.ex01;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Date;
-import java.sql.Timestamp;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -12,7 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/member")
+/*@WebServlet("/member")*/
 public class MemberServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request,HttpServletResponse response)
                                                     throws ServletException, IOException {
