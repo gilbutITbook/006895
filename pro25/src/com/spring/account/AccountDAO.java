cd50a8ec2a232cb71cedda8a8f19da1927e188bc
leebs126 <leebs126@DESKTOP-URMM4P2>
2019-05-10 18:32:29 +0900

    필요없는 코드 삭제

--- pro25/src/com/spring/account/AccountDAO.java
+++ pro25/src/com/spring/account/AccountDAO.java
@@ -11,12 +11,10 @@ public class AccountDAO {
 	}
 
 	public void updateBalance1() throws DataAccessException {
-		AccountVO accountVO = new AccountVO("70-490-930","È«±æµ¿",5000000);
 		sqlSession.update("mapper.account.updateBalance1");
 	}
 	
 	public void updateBalance2() throws DataAccessException {
-		AccountVO accountVO = new AccountVO("70-490-911","±èÀ¯½Å",5000000);
 		sqlSession.update("mapper.account.updateBalance2");
 	}
 	
