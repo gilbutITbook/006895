028bd434d0ddaca07a6c4ea55ce9efcef7d0b87d
user <user@DESKTOP-7RFE112>
2020-11-15 21:37:59 +0900

    upload() 메서드 수정

--- pro30/src/main/java/com/myspring/pro30/board/controller/BoardControllerImpl.java
+++ pro30/src/main/java/com/myspring/pro30/board/controller/BoardControllerImpl.java
@@ -342,7 +342,7 @@ public class BoardControllerImpl  implements BoardController{
 			if(mFile.getSize()!=0){ //File Null Check
 				if(!file.exists()){ //경로상에 파일이 존재하지 않을 경우
 					file.getParentFile().mkdirs();  //경로에 해당하는 디렉토리들을 생성
-					mFile.transferTo(new File(ARTICLE_IMAGE_REPO +"\\"+"temp"+ "\\"+imageFileName)); //임시로 저장된 multipartFile을 실제 파일로 전송
+					mFile.transferTo(new File(ARTICLE_IMAGE_REPO +"\\"+"temp"+ "\\"+originalFileName)); //임시로 저장된 multipartFile을 실제 파일로 전송
 				}
 			}
 		}
