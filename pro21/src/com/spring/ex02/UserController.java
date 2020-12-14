33e2449ac79a217a64c85581e910c46ad66be657
user <user@DESKTOP-7RFE112>
2020-12-14 12:06:57 +0900

    getViewName 메서드 주석 추가

--- pro21/src/com/spring/ex02/UserController.java
+++ pro21/src/com/spring/ex02/UserController.java
@@ -60,27 +60,30 @@ public class UserController extends MultiActionController {
 	      if(uri == null || uri.trim().equals("")) {
 	         uri = request.getRequestURI();
 	      }
-
-	      int begin = 0;
+	      
+	      //http://localhost:8090/member/listMember.do로 요청시
+	      int begin = 0;  //
 	      if(!((contextPath==null)||("".equals(contextPath)))){
-	         begin = contextPath.length();
+	         begin = contextPath.length();  // 전체 요청명 의 길이를 구함
 	      }
 
 	      int end;
 	      if(uri.indexOf(";")!=-1){
-	         end=uri.indexOf(";");
+	         end=uri.indexOf(";");  //요청 uri에 ';'가 있을 경우 ';'문자 위치를 구함
 	      }else if(uri.indexOf("?")!=-1){
-	         end=uri.indexOf("?");
+	         end=uri.indexOf("?");   //요청 uri에 '?'가 있을 경우 '?' 문자 위치를 구함
 	      }else{
 	         end=uri.length();
 	      }
 
+	      //http://localhost:8090/member/listMember.do로 요청시 먼저 '.do'를 제거한 http://localhost:8090/member/listMember를 구한 후,
+	      //다시 http://localhost:8090/member/listMember에서 역순으로 첫번째 '/' 위치를 구한 후, 그 뒤의 listMember를 구한다.
 	      String fileName=uri.substring(begin,end);
 	      if(fileName.indexOf(".")!=-1){
-	         fileName=fileName.substring(0,fileName.lastIndexOf("."));
+	         fileName=fileName.substring(0,fileName.lastIndexOf("."));  //요청명에서 역순으로 최초 '.'의 위치를 구한후, '.do' 앞에까지의 문자열을 구함
 	      }
 	      if(fileName.lastIndexOf("/")!=-1){
-	         fileName=fileName.substring(fileName.lastIndexOf("/"),fileName.length());
+	         fileName=fileName.substring(fileName.lastIndexOf("/"),fileName.length()); //요청명에서 역순으로 최초 '/'의 위치를 구한후, '/' 다음부터의 문자열을 구함  
 	      }
 	      return fileName;
 	   }
